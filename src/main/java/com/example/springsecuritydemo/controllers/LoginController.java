5f7c17daa4e78e04c7d9b81d7159573ddd2b5a4b
Aneeq Anwar <aneeq.anwar@confiz.com>
2023-03-28 02:03:31 +0500

    Added BCrypt password encoder for password hashing

--- src/main/java/com/example/springsecuritydemo/controllers/LoginController.java
+++ src/main/java/com/example/springsecuritydemo/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.example.springsecuritydemo.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,17 @@ public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity<String> response = null;
 
         try {
+            String hashedPassword = passwordEncoder.encode(customer.getPwd());
+           customer.setPwd(hashedPassword);
             savedCustomer = customerRepository.save(customer);
 
             if (savedCustomer.getId() > 0) {
