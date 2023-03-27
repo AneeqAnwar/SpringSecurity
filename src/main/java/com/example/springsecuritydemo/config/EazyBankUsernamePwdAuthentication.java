df8db8567dbbf24b6b9304e75762fc0e1c28da0d
Aneeq Anwar <aneeq.anwar@confiz.com>
2023-03-28 02:31:44 +0500

    Added custom AuthenticationProvider

---
+++ src/main/java/com/example/springsecuritydemo/config/EazyBankUsernamePwdAuthentication.java
@@ -0,0 +1,55 @@
+package com.example.springsecuritydemo.config;
+
+import com.example.springsecuritydemo.model.Customer;
+import com.example.springsecuritydemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EazyBankUsernamePwdAuthentication implements AuthenticationProvider {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        List<Customer> customers = customerRepository.findByEmail(username);
+
+        if (!customers.isEmpty()) {
+            if (passwordEncoder.matches(password, customers.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            }
+            else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }
+        else {
+            throw new BadCredentialsException("No user registered with these details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
