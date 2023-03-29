fd658091391805ee25fbbaebceb7fb3251f03331
Aneeq Anwar <aneeq.anwar@confiz.com>
2023-03-30 03:47:23 +0500

    Added CORS configurations

--- src/main/java/com/example/springsecuritydemo/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritydemo.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,20 +16,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200/"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                })
+                .and()
+                .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/contact", "/notices", "/register").permitAll();
-        http.formLogin();
-        http.httpBasic();
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
+                .requestMatchers("/contact", "/notices", "/register").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
         return http.build();
     }
 
